462e3b447080045738fce344d60b8b20dd391243
peace <peacebassey30@gmail.com>
2023-01-31 01:22:07 +0100

    new

---
+++ src/component/Cart.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import data from '../RE-use/data'
+
+const Cart = () => {
+    const [cart, setCart]=useState([])
+    const [products, setProducts]=useState([
+        data=productdata
+    ])
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Cart
