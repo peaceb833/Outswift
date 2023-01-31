77fb4a5985640cba945ecdcda257ffe25a969ff5
peace <peacebassey30@gmail.com>
2023-02-01 00:14:08 +0100

    new

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
 <div className=' footer bg-secondary   '>
    <div className=" container row">
         <div class="re  col-6 col-md-4 ">
-          <p class="last-title mx-5 h6  text-white fw-bold ">Quick Links</p>
+          <p class="last-title text-center small  text-white fw-bold ">Quick Links</p>
           <ul class="remove mt-3 text-white liststyle-none small ">
             <Link to='/' className="info text-white"> <li className='mx-3'>About Us</li></Link>
            <Link to='/'className="info text-white"><li className='mx-3'>Pricing</li></Link>
@@ -19,7 +19,7 @@ const Footer = () => {
 </ul>
 </div>
         <div class=" re col-6 col-md-4  ">
-          <p class=" text-white fw-bold mx-5 ">Our Service</p>
+          <p class=" text-white fw-bold text-center small">Our Service</p>
           <ul class="remove text-left  small">
          <Link to='/' className="info  text-white"> <li>Laundry Service</li></Link>
          <Link to='/'  className="info  text-white"><li>Dry Cleaning Service</li></Link>
@@ -32,7 +32,7 @@ const Footer = () => {
          
 </ul>
         </div>
-        <div class=" col-6 col-md-4  re"><p class="last-title text-white fw-bold mx-5"> Work with us</p>
+        <div class=" col-6 col-md-4  re"><p class="last-title text-white fw-bold small text-center"> Work with us</p>
         <ul class="remove mt-3 mx-3 small">
            <Link to='/' className="info  text-white"> <li>Careers</li></Link>
            <Link to='/'  className="info  text-white"><li>Partner drivers</li></Link>
@@ -42,7 +42,7 @@ const Footer = () => {
 
 
           </ul></div>
-        <div class=" re col-6 col-md-4"><p class="last-title text-white fw-bold mx-4">Explore</p>
+        <div class=" re col-6 col-md-4"><p class="last-title text-white fw-bold  mx-4 small">Explore</p>
           <ul class="remove mt-3 small">
           <Link to='/' className="info  text-white "> <li>How it works</li></Link>
           <Link to='/'  className="info  text-white"><li>Prices & Services</li></Link>
