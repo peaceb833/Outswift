fff251e99d9e7c12be3007dc87a7e1efc8fbdd34
peace <peacebassey30@gmail.com>
2023-02-01 22:34:02 +0100

    new

--- src/RE-use/Call.js
+++ src/RE-use/Call.js
@@ -4,11 +4,14 @@ import ChatIcon from '@material-ui/icons/Chat';
 import "../Css/call.css"
 const Call = () => {
   return (
-    <div className='text-end  mb-5 pb-5 fw-bold h1 fixed-bottom   '>
+    <div className='text-end mb-5 pb-5 fw-bold h1 fixed-bottom   '>
+      <h1>
     <Link to='Time' className='text-center '>
-        <ChatIcon className='  text-white  bg-primary fonty 'fontSize='large'></ChatIcon>
+      
+        <ChatIcon className=' p-1 text-white  bg-primary fonty 'fontSize='large'/>
+        
     </Link>
-
+</h1>
     </div>
   )
 }
